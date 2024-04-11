68e76509f1ee9d0bfea547eacdcd745ddb1e6a97
Jason G. Wallace <jason.wallace@uga.edu>
2024-04-11 10:19:22 -0400

    Update 3c to export CSV of beta diversity GxE

--- revamp_pipeline/3c_CalcGxE_BetaDiversity.r
+++ revamp_pipeline/3c_CalcGxE_BetaDiversity.r
@@ -4,7 +4,6 @@
 
 # TODO: DBRDA supposed to use distance matrix, but giving it first 2 PCs instead.
 #    That seems wrong. Correct?
-# TODO: Export a text table of values (with p-values); may need for later analyses
 # TODO: Confirm that dbrda correctly matches distance matrix with values based on sample name
 
 library(ggplot2)
@@ -14,7 +13,8 @@ library(tidyverse)
 library(vegan)
 
 # Global variables
-nperms = 1000 # number of permutations for distance-based redundancy analysis
+nperms = 999 # number of permutations for distance-based redundancy analysis
+set.seed(1) # Hoping this works for anova.cca() permutations; documentation unclear, but tests indicate should work
 
 # Load necessary data
 weighted_unifrac = read_qza("0_data_files/weighted_unifrac_distance_matrix.qza")
@@ -42,27 +42,11 @@ beta_heritability_cal <- function(input_qza, input_metadata) {
   
   # Run dbRDA and analyze
   dbRDA_result <- dbrda(distance_matrix ~ location + Corrected_pedigree + location:Corrected_pedigree, input_metadata)
-  anova_result <- anova(dbRDA_result, by = "terms", permu = nperms)
+  anova_result <- anova(dbRDA_result, by = "terms", permutations = nperms)
   
   # Calculate heritability and create a dataframe for the results
-  heritability_df <- heritability_calculation_beta(anova_result)
-  
-  return(heritability_df)
-}
-
-#########################################################################
-
-# Function to calculate heritability based on ANOVA results
-heritability_calculation_beta <- function(anova_result) {
-  ssq_total <- sum(anova_result$SumOfSqs)
-  heritability <- anova_result$SumOfSqs / ssq_total
-  
-  # Create a dataframe with heritability results
-  heritability_df <- data.frame(
-    Factor = rownames(anova_result),
-    Heritability = heritability,
-    stringsAsFactors = FALSE
-  )
+  heritability_df <- data.frame(anova_result, check.names=FALSE) %>%
+    mutate(heritability = SumOfSqs / sum(SumOfSqs))
   
   return(heritability_df)
 }
@@ -71,18 +55,18 @@ heritability_calculation_beta <- function(anova_result) {
 
 # Function to construct contribution tables from heritability data
 construct_contribution_table <- function(heritability_df, type) {
-  # Extract heritability values for Environment, Maize Genotype, and GXE
-  environment_heritability <- heritability_df$Heritability[heritability_df$Factor == "location"]
-  maize_genotype_heritability <- heritability_df$Heritability[heritability_df$Factor == "Corrected_pedigree"]
-  gxe_heritability <- heritability_df$Heritability[heritability_df$Factor == "location:Corrected_pedigree"]
-  
-  # Construct a dataframe for contribution data
-  contribution_df <- data.frame(
-    test_var = c("Environment", "Maize Genotype", "GXE"),
-    Value = c(environment_heritability, maize_genotype_heritability, gxe_heritability),
-    Type = type,
-    stringsAsFactors = FALSE
-  )
+
+  contribution_df = heritability_df %>%
+    rownames_to_column("factor") %>%
+    mutate(Type = type) %>%
+    mutate(test_var = case_when(factor == "location" ~ "Environment",
+                                factor == "Corrected_pedigree" ~ "Maize Genotype",
+                                factor == "location:Corrected_pedigree" ~ "GXE",
+                                factor == "Residual" ~ "Residual",
+                                TRUE ~ "UNKNOWN") ) %>%
+    relocate(test_var, Type) %>%
+    select(-factor) %>%
+    filter(test_var != "Residual")
   
   return(contribution_df)
 }
@@ -102,13 +86,9 @@ unifrac_contribution_table <- dplyr::bind_rows(weighted_unifrac_contribution_tab
 
 # Prepare for combined alpha and beta diversity analysis
 unifrac_contribution_table$category <- "Beta Diversity"
-#colnames(alpha_result)[1] <- "Type"  # Ensure this aligns with the 'Type' column in the UniFrac tables
-
-# Combine alpha and beta diversity contribution tables
-#alpha_beta_diversity_table <- dplyr::bind_rows(unifrac_contribution_table, alpha_result)
 
 # Plot the contributions of G, E, and GXE on alpha and beta diversity
-betaplot = ggplot(data = unifrac_contribution_table, aes(x = Type, y = Value, fill = test_var)) +
+betaplot = ggplot(data = unifrac_contribution_table, aes(x = Type, y = heritability, fill = test_var)) +
   geom_bar(position = "stack", stat = "identity") +
   ylab("Variance Explained") +
   ggtitle("GxE for Beta Diversity") + 
@@ -117,3 +97,6 @@ betaplot = ggplot(data = unifrac_contribution_table, aes(x = Type, y = Value, fi
 
 # Save the plot
 ggsave(betaplot, file="3_GxE/3c_beta_diversity_GxE.png", height = 3, width = 3, device = "png")
+
+# Save the output data 
+write.csv(unifrac_contribution_table, file="3_GxE/3c_beta_diversity_GxE.csv", row.names=FALSE)
