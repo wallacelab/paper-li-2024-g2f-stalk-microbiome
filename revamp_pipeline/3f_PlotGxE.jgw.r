048880cb99b5c4d9048f8311e1679cfa4ec131ca
Jason G. Wallace <jason.wallace@uga.edu>
2024-12-03 11:58:13 -0500

    Combine & polish GxE plots

--- revamp_pipeline/3f_PlotGxE.jgw.r
+++ revamp_pipeline/3f_PlotGxE.jgw.r
@@ -9,11 +9,12 @@ library(ggpubr)
 alpha = read.csv("3_GxE/3b_alpha_diversity.variance_explained.jgw.csv")
 beta = read.csv("3_GxE/3c_beta_diversity_GxE.jgw.csv")
 pathways = read.csv("3_GxE/3d_MetaCyc_pathway_GXE.jgw.csv")
-# taxonomy = TODO
+taxonomy = read.csv("3_GxE/3e_taxon_GXE.jgw.csv")
 
 # Global variables
 sig_threshold=0.05 # Significance threshold
 colorkey = c("Maize Genotype" = "#74afda", "GXE" = "#00a693", "Environment" = "#da9100")
+taxa_levels=c("Phylum", "Class","Order","Family","Genus","Species")
 
 #########
 # Alpha and Beta Diversity
@@ -40,10 +41,12 @@ div_barplot = function(plotdata, set="???"){
     ylab("Variance Explained") +
     ggtitle(paste(set, "Diversity")) + 
     theme_minimal() +
-    theme(legend.position = "bottom", 
+    theme(legend.position = "none", 
           axis.title.x = element_blank(), 
           legend.title = element_blank(), 
-          axis.text.x = element_text(angle=90)) +
+          axis.text.x = element_text(angle=90, vjust=0.5, hjust=1),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor = element_blank()) +
     scale_alpha(guide='none', range=c(0.3,1)) + # Use to set how transparent becomes
     scale_fill_manual(values=colorkey) + 
     geom_text(alpha=1, position=position_stack(vjust=0.5), size=2)
@@ -60,27 +63,85 @@ betaplot = div_barplot(beta, set="Beta")
 # Significance
 path.significant = pathways %>%
   group_by(term) %>%
-  summarize(fraction = sum(fdr < 0.01)/n()) %>%
-  mutate(percent = round(fraction*100, digits=1) %>% paste("% sig."))
+  summarize(n=n(), fraction = sum(pval < 0.01)/n(), .groups="drop") %>%
+  mutate(percent = round(fraction*100, digits=1) %>% paste("%", sep=""))
 
 # Make violin plot  
 pathplot = ggplot(pathways) +
   aes(x = term, y = herit, fill = term) +
   geom_violin(trim = TRUE, linewidth=0.25) +
   stat_summary(fun = mean, geom = "crossbar", width = 0.2, color = "black") +
-  geom_text(data=path.significant, mapping=aes(label=percent), y=max(pathways$herit),
-            size=3) +
+  geom_text(data=path.significant, mapping=aes(label=percent), 
+            size=2.5, y=max(pathways$herit)* 1.06) +
+  geom_text(data=path.significant, mapping=aes(label=n), 
+            size=2, y=max(pathways$herit * 1.02)) +
   ylab("Variance Explained") +
   ggtitle(paste("Metabolism (inferred)")) + 
   theme_minimal() +
   theme(legend.position = "none", 
         axis.title.x = element_blank(), 
         legend.title = element_blank(), 
-        axis.text.x = element_text(angle=90)) +
+        axis.text.x = element_text(angle=90, vjust=0.5, hjust=1),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor=element_blank()) +
+  scale_fill_manual(values=colorkey) +
+  ylim(NA, max(pathways$herit) * 1.1) # Adjust vertical limit
+
+# geom_text(data=sig.taxa, mapping=aes(label=percent), 
+#           size=2.5, y=max(taxonomy$herit)* 1.06) +
+#   geom_text(data=sig.taxa, mapping=aes(label=n), 
+#             size=2, y=max(taxonomy$herit) * 1.02) +
+
+
+
+##############
+# Taxonomy
+##############
+
+
+# Calculate fractions that are significant
+sig.taxa = taxonomy %>%
+  group_by(level, term) %>%
+  summarize(n=n(), fraction = sum(pval < 0.01)/n(), .groups="drop") %>%
+  mutate(percent = round(fraction*100, digits=1) %>% paste("%", sep=""))
+
+
+# Plot
+taxonomy$level = factor(taxonomy$level, levels=taxa_levels)
+taxaplot <- ggplot(taxonomy) +
+  aes(x=level, y=herit, fill=term) +
+  geom_violin(trim = TRUE, linewidth=0.25) +
+  ylab("Variance Explained") + 
+  ggtitle(paste("Taxonomy")) + 
+  theme_minimal() +
+  theme(legend.position = "bottom", 
+        axis.title.x = element_blank(), 
+        legend.title = element_blank(), 
+        axis.text.x = element_text(angle=90, vjust=0.5, hjust=1),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank()) +
+  facet_grid(~term) +
+  # theme(strip.text.x = element_text(size = 30,face="bold"),
+  #       strip.text.y = element_text(size = 30,face="bold" )) + 
+  stat_summary(fun = "mean", geom = "crossbar",  width = 0.5, colour = "black",
+               show.legend=FALSE) +
+  geom_text(data=sig.taxa, mapping=aes(label=percent), 
+            size=2.5, y=max(taxonomy$herit)* 1.06) +
+  geom_text(data=sig.taxa, mapping=aes(label=n), 
+            size=2, y=max(taxonomy$herit) * 1.02) +
+  ylim(NA, max(taxonomy$herit) * 1.1) + # Adjust vertical limit
   scale_fill_manual(values=colorkey)
 
+
+
+########################
 # Combine plots and save
-top = ggarrange(alphaplot, betaplot, nrow=1, labels=c("A","B"))
-bottom = ggarrange(pathplot, nrow=1, ncol=2, labels=c("C","D"))
+########################
+
+top = ggarrange(alphaplot, betaplot, pathplot, nrow=1, labels=c("A","B", "C"),
+                align="h")
+bottom = ggarrange(taxaplot, labels=c("D"))
 combined = ggarrange(top, bottom, nrow=2, common.legend=TRUE)
-ggsave(combined, file="3_GxE/3f_GxE_combined_plot.jgw.png", width = 6, height = 8)
+ggsave(combined, file="3_GxE/3f_GxE_combined_plot.jgw.png", width = 7, height = 8)
+
+
