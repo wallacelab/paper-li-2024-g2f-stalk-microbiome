acc0954f467b89647cae9e473db4595d37fbffab
Jason G. Wallace <jason.wallace@uga.edu>
2024-12-19 16:52:11 -0500

    Make publication map figure

--- revamp_pipeline/1b_PlotFieldLocations.jgw.r
+++ revamp_pipeline/1b_PlotFieldLocations.jgw.r
@@ -13,9 +13,10 @@ library(pals)
 # Load data
 mapfile="1_parsed_files/1b_map_data.rds"  # File to store map data so only have to call once
 metadata=read.csv("0_data_files/g2f_2019_field_metadata.csv")
+raw_locs = read.delim("0_data_files/G2f_2019_sub_corrected_metadata.tsv")$location %>% 
+  unique() %>% na.omit() %>% as.character()  # Pull out all original locations
 filtered = readRDS("1_parsed_files/1a_asv_table_no_taxa_from_blanks.rarefy1500.yellow_stripe.phyloseq.rds")
 filtered_locs = sample_data(filtered)$location %>% unique()
-
 
 
 #########
@@ -34,8 +35,9 @@ plotdata=metadata %>%
 plotdata$latitude[plotdata$location=="NEH2"] = plotdata$latitude[plotdata$location=="NEH1"]
 plotdata$longitude[plotdata$location=="NEH2"] = plotdata$longitude[plotdata$location=="NEH1"]
 
-# Determine if sampled
-plotdata$sampled = plotdata$location %in% filtered_locs
+# Determine if sampled & if passed filters
+plotdata$sampled = plotdata$location %in% raw_locs
+plotdata$pass_filters = plotdata$location %in% filtered_locs
 
 # Group into regions
 plotdata = plotdata %>% mutate(region = case_when(
@@ -47,16 +49,17 @@ plotdata = plotdata %>% mutate(region = case_when(
 ))
 
 # Define location color key
-ncolors = sum(plotdata$sampled)
+ncolors = sum(plotdata$pass_filters)
 plotdata$location_color=NA
+plotdata$location_color[!plotdata$pass_filters]="black" # Black for ones sampled but filtered out
 plotdata$location_color[!plotdata$sampled]="gray50" # Gray for unsampled locations
-plotdata$location_color[plotdata$sampled]= pals::kelly()[1:ncolors + 2]  # +2 offset to avoid light gray & make higher contrast among sites in same region
+plotdata$location_color[plotdata$pass_filters]= pals::kelly()[1:ncolors + 2]  # +2 offset to avoid light gray & make higher contrast among sites in same region
 ## Make a variable for use later
 lockey = plotdata$location_color
 names(lockey) = plotdata$location
 
 # Define region color key; colorblind-friendly palette from https://davidmathlogic.com/colorblind/
-regionkey = c("Midwest (East)" = "#FFC107",
+regionkey = c("Midwest (East)" = "#cd9a00",
               "Midwest (West)" = "#D81B60", 
               "Northeast" = "#1E88E5", 
               "South" = "#004D40", 
@@ -85,16 +88,16 @@ if(file.exists(mapfile)){
 states=map_data("state")
 
 # Helper function to make base map and standard stuff
-basemap = function(m){
+basemap = function(m, xlim=c(-110, -70), ylim=c(25, 47)){
   ggmap(m) +
     geom_polygon(mapping=aes(x=long, y=lat, group=group), data=states, fill="#00000000", color="black") +
-    coord_map(projection="mercator", xlim=c(-110, -70), ylim=c(25, 47)) +
+    coord_map(projection="mercator", xlim=xlim, ylim=ylim) +
     labs(x="Longitude", y="Latitude")
 }
 
 # Standard parameters
-sampled = plotdata %>% filter(sampled==TRUE)
-pointsize=2.5
+final = plotdata %>% filter(pass_filters==TRUE)
+pointsize=3
 pointshape=21
 pointstroke=0.5
 
@@ -106,9 +109,9 @@ regionplot = basemap(mymap) +
 ggsave(regionplot, file="1_parsed_files/1b_g2f_locations.by_region.png", width=5, height=4)  
 
 # Map colored by region (Sampled)
-regionplot = basemap(mymap) +
+regionplot = basemap(mymap, xlim=c(-100, -70), ylim=c(29, 47)) +
   geom_point(mapping=aes(x=longitude, y=latitude, fill=region), 
-             data=sampled, size=pointsize, shape=pointshape, stroke=pointstroke) +
+             data=final, size=pointsize, shape=pointshape, stroke=pointstroke) +
   scale_fill_manual(values=regionkey)
 ggsave(regionplot, file="1_parsed_files/1b_g2f_locations.by_region.sampled.png", width=5, height=4)  
 
@@ -121,9 +124,38 @@ locationplot = basemap(mymap) +
 ggsave(locationplot, file="1_parsed_files/1b_g2f_locations.by_location.png", width=5, height=4)  
 
 # Subset to just the sampled locations
-sampleplot = basemap(mymap) +
+sampleplot = basemap(mymap, xlim=c(-100, -70), ylim=c(29, 47)) +
   geom_point(mapping=aes(x=longitude, y=latitude, fill=location), 
-             data=sampled, size=pointsize, shape=pointshape, stroke=pointstroke) +
+             data=final, size=pointsize, shape=pointshape, stroke=pointstroke) +
   scale_fill_manual(values=lockey)
 ggsave(sampleplot, file="1_parsed_files/1b_g2f_locations.by_location.sampled.png", width=5, height=4)  
+
+
+################
+# Publication Graphic
+################
+
+# Adjustments
+final$abbreviation = sub(final$location, pattern="H.$", repl="")
+regions = data.frame(region = names(regionkey), 
+                     region_color=regionkey,
+                     latitude =c( 38.5,    37.7,    40.7,   34.0,   0),  # 0 is for "other"
+                     longitude=c(-85,  -94.6,      -76,  -79.5,   0))
+  
+  
+  # final %>%
+  # group_by(region, region_color) %>%
+  # summarize(latitude = mean(latitude), longitude=mean(longitude), .groups="drop")
+
+# Final graphic for publication
+pubplot = basemap(mymap, xlim=c(-100, -70), ylim=c(29, 47)) +
+  geom_point(mapping=aes(x=longitude, y=latitude, fill=region), 
+             data=final, size=3.5, shape=21, stroke=0.5) +
+  scale_fill_manual(values=regionkey) +
+  geom_text(mapping=aes(x=longitude, y=latitude, label=abbreviation), 
+            data=final, color="white", size=1.5, fontface="bold") +
+  annotate(geom="label", x=regions$longitude, y=regions$latitude, fill=regions$region_color,
+           label=regions$region, color = "white", fontface = "bold", size=3, alpha=0.8) +
+  theme(legend.position="none")
+ggsave(pubplot, file="1_parsed_files/1b_g2f_locations.publication.png", width=4, height=4)  
 
