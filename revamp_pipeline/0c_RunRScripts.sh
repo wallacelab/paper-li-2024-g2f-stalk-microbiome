1195f7c4fb282c35438f3e2b54d241434ebca575
Jason G. Wallace <jason.wallace@uga.edu>
2025-06-11 11:57:03 -0400

    Remove deleted files from tracking

--- revamp_pipeline/0c_RunRScripts.sh
+++ revamp_pipeline/0c_RunRScripts.sh
@@ -12,29 +12,30 @@ if [[ ! -e 5_Associations/5d_pathways ]] ; then mkdir 5_Associations/5d_pathways
 
 
 # Basic data unification and cleaning
-Rscript 1a_UnifyData.r
-Rscript 1b_PlotFieldLocations.r
+# Rscript 1a_UnifyData.r
+# Rscript 1b_PlotFieldLocations.r
 
 # Alpha and beta diversity
-Rscript 2a_AlphaDiversity.r
+# Rscript 2a_AlphaDiversity.r
 Rscript 2b_BetaDiversity_PCOA.r
-
-# GxE Breakdown
-Rscript 3a_FilterSamplesForGxE.r
-Rscript 3b_CalcGxE_AlphaDiversity.r
-Rscript 3c_CalcGxE_BetaDiversity.r
-Rscript 3d_CalcGxe_Pathways.r
-Rscript 3e_CaclGxE_Taxonomy.r
-Rscript 3e1_CompareGxE_Taxonomy.r
-Rscript 3f_PlotGxE.r
-
-# Core microbiome
-Rscript 4a_CoreMicrobiome.r
-Rscript 4d_SummarizeCoreTaxa.r
-
-# Environmental associations
-Rscript 5a_AlphaDiversityAssociation.r
-Rscript 5b_BetaDiversityAssociation.r
-Rscript 5c_BetaDiversityAssociation_GenotypeLocation.r
-Rscript 5d_PathwayAssociation.r
+# Rscript 2c_BetaDiversity_Significance.r
+# 
+# # GxE Breakdown
+# Rscript 3a_FilterSamplesForGxE.r
+# Rscript 3b_CalcGxE_AlphaDiversity.r
+# Rscript 3c_CalcGxE_BetaDiversity.r
+# Rscript 3d_CalcGxe_Pathways.r
+# Rscript 3e_CaclGxE_Taxonomy.r
+# Rscript 3e1_CompareGxE_Taxonomy.r
+# Rscript 3f_PlotGxE.r
+# 
+# # Core microbiome
+# Rscript 4a_CoreMicrobiome.r
+# Rscript 4d_SummarizeCoreTaxa.r
+# 
+# # Environmental associations
+# Rscript 5a_AlphaDiversityAssociation.r
+# Rscript 5b_BetaDiversityAssociation.r
+# Rscript 5c_BetaDiversityAssociation_GenotypeLocation.r
+# Rscript 5d_PathwayAssociation.r
 
